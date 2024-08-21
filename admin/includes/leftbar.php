15d7a5c4789eb36d29ad761fed0bbbef01fe02a2
musfiqur24 <musfiqur.utsho@gmail.com>
2024-08-22 00:46:24 +0800

    Admin

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,22 @@
+	<nav class="ts-sidebar">
+		<ul class="ts-sidebar-menu">
+
+			<li class="ts-label">Main</li>
+			<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+			<li><a href="manage-brands.php"><i class=" fa fa-files-o"></i> Brands</a></li>
+
+
+
+
+
+			<li><a href="manage-vehicles.php"><i class="fa fa-sitemap"></i> Vehicles</a></li>
+			<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+			<li><a href=" reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+
+
+
+
+
+		</ul>
+	</nav>
---
+++ admin/index.php
@@ -0,0 +1,195 @@
+<?php
+session_start();
+include('includes/config.php');
+if (isset($_POST['login'])) {
+	$email = $_POST['Username'];
+	$Password = $_POST['Password'];
+	$sql = "SELECT Username,Password FROM admin WHERE Username=:email and Password=:Password";
+	$query = $dbh->prepare($sql);
+	$query->bindParam(':email', $email, PDO::PARAM_STR);
+	$query->bindParam(':Password', $Password, PDO::PARAM_STR);
+	$query->execute();
+	$results = $query->fetchAll(PDO::FETCH_OBJ);
+	if ($query->rowCount() > 0) {
+		$_SESSION['alogin'] = $_POST['Username'];
+
+		echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+	} else {
+
+		echo "<script type='text/javascript'> document.location = 'manage-brands.php'; </script>";
+		// echo "<script>alert('Invalid Details');</script>";
+	}
+}
+
+
+?>
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Car2Go | Admin Login</title>
+	<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #3976a475
+}
+
+.container {
+  width: 400px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #ffffff
+}
+
+.input-group {
+  margin-bottom: 15px;
+}
+
+.label {
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="password"]{
+  width: calc(100% - 22px);
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+input[type="submit"]{
+    background-color: #f79131;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    margin-left: 10px;
+    margin-right:10px;
+    font-size: 16px; 
+}
+
+button {
+    background-color: #f79131;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    margin-left: 10px;
+    margin-right:10px;
+    font-size: 16px; 
+}
+button:hover{
+    background-color: #ed8421;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+}
+a.signup-button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  a.signup-button:hover {
+    background-color: #45a049;
+    text-decoration: none;
+  }
+
+a {
+  text-decoration: none;
+  color: #007bff;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.error {
+  color: red;
+  margin-bottom: 10px;
+}
+
+.success {
+  color: green;
+  margin-bottom: 10px;
+}
+.previous{
+    text-decoration: none; 
+    display: inline-block; 
+    padding: 8px 16px; 
+    background-color: #f1f1f1; 
+    color: black; 
+}
+
+.previous:hover{
+    text-decoration: none; 
+    display: inline-block; 
+    padding: 8px 16px; 
+    background-color: #e3e3e3; 
+    color: black;
+}
+</style>
+</head>
+
+<body>
+
+<div class="container">
+  <h2>Admin Login</h2>
+  <form method="post">
+      <div class="input-group">
+      <label for="username">Username</label><br>
+      <input type="text" id="Username" name="Username" required>
+    </div>
+    <div class="input-group">
+      <label for="Password">Password</label><br>
+      <input type="Password" id="Password" name="Password" required><br>
+    </div>
+    <div class="input-group">
+      <input type="submit" name="login" value="Login">
+    </div><br>
+    <center><div class="input-group">
+      <a href="../index.php" class="previous">&laquo; Back to Homepage</a>
+    </div></center>
+  </form>
+</div>
+								
+
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
